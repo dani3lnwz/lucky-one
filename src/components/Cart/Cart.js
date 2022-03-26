8c67c62e7ffe74b1748b61079cbd33b8e2ac502c
Daniel newaz <danielnwz420@gmail.com>
2022-03-26 14:54:30 +0600

    selectin

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
-    console.log(cart)
+    console.log(cart);
+
+    const choseOne = car => {
+        if (cart.length) {
+            const randomNumber = Math.round(Math.random() * (cart.length -1));
+            console.log(randomNumber);
+            const text = cart[randomNumber].name +  ' Suggested For You!';
+            alert(text);
+        }
+        else{
+            alert('Please select at least one item')
+        }
+    }
+
     return (
         <div className='detail'>
               <div className="cart-container">
-              const selected = cart[Math.floor(Mat.random()*cart.length)];
                   
                     <h3>Order Summary in</h3>
                     <p>Selected Items: {cart.length}</p>
@@ -16,7 +28,7 @@ const Cart = ({cart}) => {
                       ))
                     }
                     
-                    <button>Chose One For Me</button>
+                    <button onClick={() => choseOne(cart)}>Chose One For Me</button>
                 </div>
         </div>
     );
