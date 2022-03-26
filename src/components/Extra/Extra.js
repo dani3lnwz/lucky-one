b20742d679529edb57426d7de513fc2d291e359d
Daniel newaz <danielnwz420@gmail.com>
2022-03-26 22:33:11 +0600

    ans-updt

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Extra.css'
 
 const Extra = () => {
     return (
-        <div>
+        <div className='Ans'>
             <h5>How React Works?</h5>
-            <p>React does not commit state changes one after the other,if there are multiple state changes. Instead, React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process. In fancy words, React does batch updates.</p>
+            <p>React worksin declarative way.Declaretive means developers don't need to mentioned how this code will work rather than developer only write expressions fow what we want.React is a javascript library,Javascript  ads interectivity to our website and make it eaiser.React use declarative code to create components.which is give us opportinity to show data called information dynamiclly. React does not commit state changes one after the other,if there are multiple state changes. In fancy words, React does batch updates.</p>
             <h5>Difrence between props and state.</h5>
             <p>props: <br />
             1:props are read only. <br />
